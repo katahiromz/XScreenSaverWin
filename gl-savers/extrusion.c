91fd143ca79876e021bb56fe5fcd554cb10b1401
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2017-11-23 18:41:38 +0900

    fix extrusion

--- gl-savers/extrusion.c
+++ gl-savers/extrusion.c
@@ -28,12 +28,6 @@
 	#endif
 #endif
 
-#ifdef NDEBUG
-	#pragma comment(lib, "gle.lib")
-#else
-	#pragma comment(lib, "gled.lib")
-#endif
-
 #define DELAY 20000
 #define	DEFAULTS	"*delay:	 20000	\n" \
 					"*showFPS:	 False	\n" \
