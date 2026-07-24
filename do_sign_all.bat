54d802399c1d33b09e65de60102d556ea4c852ac
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2026-07-24 15:39:19 +0900

    fix do_sign_all.bat

--- do_sign_all.bat
+++ do_sign_all.bat
@@ -1,5 +1,5 @@
 @echo off
-for /R %%f in (*-setup.exe build\Release\*.exe build\Release\*.scr build\Release\*.dll) do (
+for /R %%f in (*-Setup.exe build\Release\*.exe build\Release\*.scr build\Release\*.dll) do (
 	echo "%%~ff" | findstr /I /C:"\CMakeFiles\" /C:"\.git\" /C:"\.vs\" >NUL
 	if errorlevel 1 (
 		call ..\do_sign.bat "%%~ff"
