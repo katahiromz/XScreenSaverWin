881cfac1cc4e18739841b55723ac1ee0c9078344
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2026-07-26 09:45:43 +0900

    fix double buffering

--- screenhack/xdbe.c
+++ screenhack/xdbe.c
@@ -6,15 +6,12 @@ static Pixmap pixmap = NULL;
 XdbeBackBuffer 
 xdbe_get_backbuffer(Display *dpy, Window window, XdbeSwapAction action)
 {
-	pixmap = XCreatePixmap(dpy, window, ss.width, ss.height, 32);
-	return pixmap;
+    pixmap = XCreatePixmap(dpy, window, ss.width, ss.height, 32);
+    return pixmap;
 }
 
 void XdbeSwapBuffers(Display *dpy, XdbeSwapInfo *info, int count)
 {
-	HDC hdc = CreateCompatibleDC(dpy);
-	HGDIOBJ hbmOld = SelectObject(hdc, pixmap->hbm);
-	BitBlt(dpy, 0, 0, ss.width, ss.height, hdc, 0, 0, SRCCOPY);
-	SelectObject(hdc, hbmOld);
-	DeleteDC(hdc);
+    HDC hdc = XCreateDrawableDC_(dpy, pixmap);
+    BitBlt(dpy, 0, 0, ss.width, ss.height, hdc, 0, 0, SRCCOPY);
 }
